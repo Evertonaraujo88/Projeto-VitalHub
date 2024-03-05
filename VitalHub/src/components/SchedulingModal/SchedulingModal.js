fcefe56db32dafef679fa525a21efe5781959891
Usuario <evertonaraujosenai@gmail.com>
2024-03-05 00:11:17 -0300

    selecionar data e modal agendamento de consulta

--- VitalHub/src/components/SchedulingModal/SchedulingModal.js
+++ VitalHub/src/components/SchedulingModal/SchedulingModal.js
@@ -1,9 +1,63 @@
+import { ButtonSalvar } from "../../screens/ProntuarioMedico/Style";
+import { TitleSelect } from "../../screens/SelecionarClinica/Style";
+import { ButtonCancel, ButtonSecondaryTitle, ButtonTitle } from "../Botao/Style";
+import { AlignBoxModal, BoxSchedulingModal, ContentModal, ContentModalSchedulingBox, DateBox, DoctorBox, ModalScheduling, SubTitle, TextDefault, TitleTextInfo } from "./Style";
 
+export const SchedulingModal = ({
+  visible,
+  setShowModalScheduling,
+  ...rest
+}) => {
+  return (
+    <ModalScheduling
+      {...rest}
+      visible={visible}
+      transparent={true}
+      animationType="fade"
+    >
+      <ContentModal>
+        <BoxSchedulingModal>
+          <ContentModalSchedulingBox>
 
-export const SchedulingModal = ({ visible, setShowSchedulingModal, ...rest }) => {
+            <TitleSelect>Agendar consulta</TitleSelect>
 
-    
-    return(
-        <></>
-    )
-}
+            <SubTitle>
+              Consulte os dados selecionados para a sua consulta
+            </SubTitle>
+
+            <DateBox>
+              <TitleTextInfo>Data da consulta</TitleTextInfo>
+              <TextDefault>1 de Novembro de 2023</TextDefault>
+            </DateBox>
+
+            <DoctorBox>
+              <TitleTextInfo>Médico(a) da consulta</TitleTextInfo>
+              <TextDefault>Dra Alessandra</TextDefault>
+              <TextDefault>Demartologa, Esteticista</TextDefault>
+            </DoctorBox>
+
+            <DoctorBox>
+              <TitleTextInfo>Local da consulta</TitleTextInfo>
+              <TextDefault>São Paulo, SP</TextDefault>
+            </DoctorBox>
+
+            <DoctorBox>
+              <TitleTextInfo>Tipo da consulta</TitleTextInfo>
+              <TextDefault>Rotina</TextDefault>
+            </DoctorBox>
+          </ContentModalSchedulingBox>
+
+          <AlignBoxModal>
+            <ButtonSalvar>
+              <ButtonTitle>Confirmar</ButtonTitle>
+            </ButtonSalvar>
+
+            <ButtonCancel onPress={() => setShowModalScheduling(false)}>
+              <ButtonSecondaryTitle>Cancelar</ButtonSecondaryTitle>
+            </ButtonCancel>
+          </AlignBoxModal>
+        </BoxSchedulingModal>
+      </ContentModal>
+    </ModalScheduling>
+  );
+};
--- VitalHub/src/components/SchedulingModal/Style.js
+++ VitalHub/src/components/SchedulingModal/Style.js
@@ -1,1 +1,72 @@
+import { Modal } from "react-native";
 import styled from "styled-components";
+
+export const ModalScheduling = styled(Modal)`
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    
+    background-color: white;
+
+`
+
+export const ContentModal = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(0,0,0,0.6);
+`
+
+export const BoxSchedulingModal = styled.View`
+    width: 92%;
+    height: 86%;
+    background-color: white;
+    border-radius: 10px;
+    align-items: center;
+`
+
+export const ContentModalSchedulingBox = styled.View`
+    height: 74%;
+    width: 85%;
+    align-items: center;
+`
+
+export const SubTitle = styled.Text`
+    font-family: Quicksand_500Medium;
+    font-size: 16px;
+    text-align: center;
+    margin-top: 16px;
+`
+
+export const TitleTextInfo = styled.Text`
+    width: 100%;
+    color: #33303E;
+    font-family: Quicksand_600SemiBold;
+    font-size: 16px;
+      
+`
+export const TextDefault = styled.Text`
+    color: #4E4B59;
+    font-family: Quicksand_500Medium;
+    font-size: 14px;
+`
+
+export const AlignBoxModal = styled.View`
+    width: 85%;
+`
+
+export const AlignButton = styled(AlignBoxModal)`
+`
+
+export const DateBox = styled.View`
+    width: 100%;
+    margin-top: 30px;
+    gap: 8px;
+`
+
+export const DoctorBox = styled.View`
+    width: 100%;
+    margin-top: 20px;
+    gap: 8px;
+`
